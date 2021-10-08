b864603ae6092a87dbc63307ed5de5ebb96df670
Restyled.io <commits@restyled.io>
2021-10-08 04:43:07 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
-  mode : 'jit',
-  purge : [
-    './dist/*.html',
-    './src/**/*.{js,vue}',
-  ],
-  darkMode : false, // or 'media' or 'class'
-  theme : {
-    extend : {},
+  mode: "jit",
+  purge: ["./dist/*.html", "./src/**/*.{js,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
   },
-  variants : {
-    extend : {},
+  variants: {
+    extend: {},
   },
-  plugins :
-      [ require('@tailwindcss/forms'), require('@tailwindcss/typography') ],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
